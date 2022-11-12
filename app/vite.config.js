9054df3b2aabdef64aabdd11ab288025053425bb
vatsalsaglani <saglanivatsal@gmail.com>
2022-11-12 23:55:03 +0530

    added server arguments

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,7 +1,11 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 
 const config = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		host: true,
+		port: 5173
+	}
 };
 
 export default config;
